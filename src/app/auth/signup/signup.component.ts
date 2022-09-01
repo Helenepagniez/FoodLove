9764d69c0dc68c1ed69b064a2fd69ce1923665da
Helene Pagniez <helenepagniez51@gmail.com>
2022-09-01 17:51:55 +0200

    page inscription débuté

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { catchError,switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private auth: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
+  }
+
+  onSignup() {
+    const email = this.signupForm.get('email')!.value;
+    const password = this.signupForm.get('password')!.value;
+    this.auth.createUser(email, password).pipe(
+      switchMap(() => this.auth.loginUser(email, password)),
+      tap(() => {
+        this.router.navigate(['/liste']);
+      }),
+      catchError(error => {
+        return error;
+      })
+    ).subscribe();
   }
 
 }
